3e14747fae2c11750ebce77ec10ed5e860731ff3
Margot van Laar <margot.vanlaar@naturealpha.ai>
2025-06-23 13:33:16 +0100

    prompt name fixes

--- backend/retrieval_graph/prompts.py
+++ backend/retrieval_graph/prompts.py
@@ -5,13 +5,13 @@ from langsmith import Client
 client = Client()
 # fetch from langsmith
 INPUT_GUARDRAIL_SYSTEM_PROMPT = (
-    client.pull_prompt("input_guardrail")
+    client.pull_prompt("margot-na/input_guardrail")
     .messages[0]
     .prompt.template
 )
 
 ROUTER_SYSTEM_PROMPT = (
-    client.pull_prompt("router")
+    client.pull_prompt("margot-na/router")
     .messages[0]
     .prompt.template
 )
@@ -22,19 +22,19 @@ GENERATE_QUERIES_SYSTEM_PROMPT = (
 )
 
 RESEARCH_PLAN_SYSTEM_PROMPT = (
-    client.pull_prompt("researcher")
+    client.pull_prompt("margot-na/researcher")
     .messages[0]
     .prompt.template
 )
 
 IRRELEVANT_QUERY_SYSTEM_PROMPT = (
-    client.pull_prompt("respond_irrelevant_query")
+    client.pull_prompt("margot-na/respond_irrelevant_query")
     .messages[0]
     .prompt.template
 )
 
 RESPONSE_SYSTEM_PROMPT = (
-    client.pull_prompt("synthesizer")
+    client.pull_prompt("margot-na/synthesizer")
     .messages[0]
     .prompt.template
 )
